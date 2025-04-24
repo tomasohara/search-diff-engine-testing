dab896ed4a845f705b408264f1b9b8cba7797250
Aviyan Acharya <avyn.xyz@gmail.com>
2025-04-24 23:07:51 +0545

    (WIP/EXPERIMENTAL) Added script to test invoked queries; Need for class based approach and command line options

---
+++ invoke_query.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Set up the WebDriver (make sure you have the right driver installed for your browser)
+driver = webdriver.Firefox()  # You can also use Firefox, Edge, etc.
+SCRAPPYCITO_URL = "http://www.scrappycito.com:9330"
+
+
+def create_url(query):
+    return SCRAPPYCITO_URL + "/run_search?query=" + query + "&its-me=on"
+
+def extract_query_results(driver, url):
+    driver.get(url)
+    time.sleep(2)
+    search_results = driver.find_elements(By.CLASS_NAME, "cell-text")
+
+    result_list = []
+    for i in range(0, len(search_results) - 2, 3):
+        result_list.append({
+            "title": search_results[i].text,
+            "website": search_results[i + 1].text,
+            "query_terms": (search_results[i + 2].text).split("; ")
+        })
+
+    result_list.pop()
+    return result_list
+
+url = create_url("democracy at risk")
+results = extract_query_results(driver, url)
+print("Results:")
+for result in results:
+    print(f"Title: {result['title']}")
+    print(f"Website: {result['website']}")
+    print(f"Query Terms: {', '.join(result['query_terms'])}")
+    print()
+
+driver.quit()
